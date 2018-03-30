a2e4e59c76366950a472eae21361648a9328b6ff
Quan <quanhv@hblab.vn>
2018-03-30 10:18:02 +0700

    change css order

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
 
-import '../scss/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../scss/style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
